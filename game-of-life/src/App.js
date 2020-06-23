1be7eab9591b8092aa65c6380842ee8308889581
richknicks <rlynch@gmail.com>
2020-06-23 19:15:05 -0400

    completed grid and buttons created about, rules, and generation files

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
-
+import About from "./components/About";
+import Grid from "./components/Grid";
+import Generation from "./components/Generation";
+import Rules from "./components/Rules";
 import "./App.css";
 
 const App = () => {
-  return <div className="App">test</div>;
+  return (
+    <div>
+      <h1>The Game of Life</h1>
+      <Generation />
+      <Grid />
+      <Rules />
+      <About />
+    </div>
+  );
 };
 
 export default App;
---
+++ game-of-life/src/components/Rules.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Rules = () => {
+  return (
+    <div>
+      <p>Rules</p>
+    </div>
+  );
+};
+export default Rules;
